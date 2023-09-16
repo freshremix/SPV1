0adb79b1c4dd274fd1f213d2914853e86f97310b
freshremix <90133873+freshremix@users.noreply.github.com>
2023-09-16 21:44:58 +0330

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import shutil
 from dotenv import dotenv_values
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-
+os.system(f'spotdl --download-ffmpeg')
 # Load Telegram token from .env file
 config = dotenv_values(".env")
 
