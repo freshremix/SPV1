4cc43c7f467aaaf982dead6ebb8080d65962f239
freshremix <90133873+freshremix@users.noreply.github.com>
2023-09-16 22:35:49 +0330

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import requests  # Import the requests library for HTTP requests
 from dotenv import dotenv_values
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-
+os.system(f'spotdl --download-ffmpeg')
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
